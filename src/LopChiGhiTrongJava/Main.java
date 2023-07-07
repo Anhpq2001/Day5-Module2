e4d716bbae9e5f9bda1f4e48c8bd7d1c61530a0c
Phạm Quang Anh <106795680+Anhpq2001@users.noreply.github.com>
2023-07-07 13:03:40 +0700

    add LopChiGhiTrongLop

---
+++ src/LopChiGhiTrongJava/Main.java
@@ -0,0 +1,9 @@
+package LopChiGhiTrongJava;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("quang anh");
+        student.setClasses("C05");
+    }
+}
